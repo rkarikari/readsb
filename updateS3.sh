0b659872a23689c7966ed2f43c56265d8f8fb0fe
Bryan Landerman <bryan@silversmith.com>
2023-05-22 16:47:21 -0400

    First attempt at make on Amazon Linux. Updated makefile output for adsbexchange.

---
+++ updateS3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ -z ${BUCKET_NAME} ]]; then
+    echo "The destination bucket name (BUCKET_NAME) is required as an environment variables."
+else
+  REPO="readsb"
+
+  # archive old tar1090-db
+  aws s3 mv "s3://${BUCKET_NAME}/${REPO}/latest" "s3://${BUCKET_NAME}/${REPO}/$(date +%F-%H-%M-%S-%N)" --recursive
+
+  # install packages required for update script
+  yum group install "Development Tools"
+  chmod +x required_packages
+  ./required_packages
+  make readsb
+
+  # push updated db folder to S3
+  aws s3 cp readsb "s3://${BUCKET_NAME}/${REPO}/latest/" --recursive
+fi
